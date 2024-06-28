9de0b9d6e35f08807e2c0491049ea266a3ada1bc
Esdras Oliveira <92229240+EsdrasOliver@users.noreply.github.com>
2024-06-27 21:45:40 -0300

    pagina adicionar

--- src/app.py
+++ src/app.py
@@ -5,7 +5,12 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
+    tipo = request.args.get('tipo', '')
     return render_template('index.html')
+
+@app.route('/adicionar')
+def adicionar():
+    return render_template('adicionar.html')
     
 if __name__ == '__main__':
     app.run(debug=True)
